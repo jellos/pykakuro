26500bab3cceec583e8c4fb4b16276f1df939f63
Brandon Thomson (eeepc) <brandon.j.thomson@gmail.com>
2010-02-27 18:33:53 -0500

    Board processing done

--- kakuro.py
+++ kakuro.py
@@ -6,7 +6,8 @@
 # Made available under an MIT license, terms at bottom of file
 
 # Kakuro boards don't really lend themselves to being drawn in ASCII, but we'll
-# give it a shot. Here's a board the way you might see it drawn in a book:
+# give it a shot. Here's a board the way you might see it drawn in a puzzle
+# book:
 #
 #      |\ |\
 #      |7\|6\
@@ -34,14 +35,73 @@
 #
 # And here is the same puzzle encoded in the input format this program uses:
 
-sample_puzzle = ((   0 ,   0 ,(0,7),(0,6)),
-                 (   0 ,(4,4),   1 ,   1 ),
-                 ((7,0),   1 ,   1 ,   1 ),
-                 ((6,0),   1 ,   1 ,   1 ),
+sample_puzzle = (   0 ,   0 ,(0,7),(0,6),
+                    0 ,(4,4),   1 ,   1 ,
+                 (7,0),   1 ,   1 ,   1 ,
+                 (6,0),   1 ,   1 ,   1 ,
                 )
+x_size = 4
+
+###
+
+ACROSS = 0
+DOWN = 1
+
+y_size = len(sample_puzzle) / x_size
+
+a=[set() if x==1 else x for x in sample_puzzle]
+
+rows = [a[z:z+x_size] for z in range(0,len(a)-x_size+1,x_size)]
+cols = [a[z::x_size] for z in range(4)]
+
+constraints = []
+
+class MalformedBoardException(Exception): pass
+
+def process_row_or_col(row, row_or_col):
+  constraints = []
+
+  row.reverse()
+  while row:
+    cell = row.pop()
+    if cell == 0:
+      pass
+    elif cell == set():
+      raise MalformedBoardException("'1' without adjacent constraint")
+    elif type(cell) == type(()):
+      if cell[row_or_col]:
+        constraint = [cell[row_or_col]]
+        cell = row.pop()
+        if cell != set():
+          raise MalformedBoardException("Constraint without adjacent '1'")
+        constraint.append(cell)
+        try:
+          while cell == set():
+            cell = row.pop()
+            constraint.append(cell)
+        except IndexError: pass
+        constraints.append(constraint)
+    else:
+      raise MalformedBoardException("{0} not a valid token".format(cell))
+
+  return constraints
+
+for row in rows:
+  constraints.extend(process_row_or_col(row, ACROSS))
+
+for col in cols:
+  constraints.extend(process_row_or_col(col, DOWN))
+
+for c in constraints:
+  for x in range(1,len(c)):
+    c[x].update(set(range(1, c[0])))
+
+for c in constraints:
+  for x in range(1,len(c)):
+    c[x].intersection_update(set(range(1, c[0])))
+
 
 # To solve the puzzle, we call solve(sample_puzzle, constraints=False)
-
 
 # Copyright (c) 2010 Brandon Thomson
 #
