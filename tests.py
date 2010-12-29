756c609e8675d0d29e48769e1bc247ec96f5d250
Brandon Thomson <brandon.j.thomson@gmail.com>
2010-12-29 14:33:01 -0500

    Redesigned for 2.7 unittest module

--- tests.py
+++ tests.py
@@ -4,6 +4,13 @@
 import kakuro
 import puzzles
 
+import logging
+import unittest
+
+logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.INFO)
+
+# Manually verified solutions to sample puzzles
 solution_1 = [0, 0, (0, 7), (0, 6),
               0, (4, 4), 1, 3,
               (7, 0), 1, 4, 2,
@@ -36,31 +43,35 @@ solution_4 = [0, 0, 0, 0, 0, 0, (0, 16), (0, 3),
               (12, 0), 3, 8, 1, 0, 0, 0, 0,
               (10, 0), 1, 9, 0, 0, 0, 0, 0]
 
-bad_soln = [0, 0, 0, 0, 0, 0, (0, 16), (0, 3),
-            0, 0, 0, 0, 0, (8, 6), 7, 1,
-            0, (0, 16), (0, 6), 0, (14, 30), 3, 9, 2,
-            (11, 0), 9, 3, (7, 0), 6, 1, (0, 6), 0,
-            (10, 0), 7, 3, (13, 7), 8, 2, 3, (0, 16),
-            0, (14, 0), 1, 4, 9, (8, 0), 1, 7,
-            0, (0, 4), (9, 17), 2, 7, (11, 0), 2, 9,
-            (12, 0), 3, 8, 1, 0, 0, 0, 0,
-            (10, 0), 1, 9, 0, 0, 0, 0, 0]
+class TestSolutions(unittest.TestCase):
+  def test_solve_examples(self):
+    p_list = [
+      (puzzles.one,   solution_1, "one"),
+      (puzzles.two,   solution_2, "two"),
+      (puzzles.three, solution_3, "three"),
+      (puzzles.four,  solution_4, "four"),
+    ]
 
-def test_solve_one():
-  assert solution_1 == kakuro.solve(puzzles.one, 4)
+    for puzzle, solution, name in p_list:
+      puzzle.solve()
+      self.assertEqual(puzzle.data, solution,
+                       "{0} solution incorrect".format(name))
 
-def test_solve_two():
-  assert solution_2 == kakuro.solve(puzzles.two, 5)
+  def test_random_puzzle_generation(self):
+    for i in range(100):
+      k = kakuro.gen_random(10, 10, seed=i)
 
-def test_solve_three():
-  assert solution_3 == kakuro.solve(puzzles.three, 8)
+      # Will raise exception on failure
+      k.check_puzzle()
 
-def test_solve_four():
-  assert solution_4 == kakuro.solve(puzzles.four, 8)
+      # Will raise exception on failure
+      k.check_solution()
 
-def test_verify_solution():
-  assert kakuro.verify_solution(solution_1, 4)
-  assert kakuro.verify_solution(solution_2, 5)
-  assert kakuro.verify_solution(solution_3, 8)
-  assert kakuro.verify_solution(solution_4, 8)
-  assert not kakuro.verify_solution(bad_soln, 8)
+  def test_random_puzzle_solutions(self):
+    for i in range(100):
+      logging.debug("Puzzle %d", i)
+      k = kakuro.gen_random(10, 10, seed=i, is_solved=False)
+      k.solve()
+
+      # Will raise exception on failure
+      k.check_solution()
